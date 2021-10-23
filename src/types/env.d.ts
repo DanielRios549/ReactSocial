1b17d2fad5339145f18e3e762d28514001fe1429
Daniel Rios <danielrios.flamengo@gmail.com>
2021-10-23 11:22:12 -0300

    Add responsive styles in separate files

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,8 +1,6 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            GITHUB_AUTH_TOKEN: string
-            NODE_ENV: 'development' | 'production'
             GITHUB_CLIENT_ID: string
             GITHUB_CLIENT_SECRET: string
             DATO_FULL_TOKEN: string
