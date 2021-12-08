1e94a0f7c859f627d78b33848ae7254927d9f9a9
Daniel Rios <danielrios.flamengo@gmail.com>
2021-12-08 08:53:52 -0300

    Remove Middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 import Header from '../src/parts/header'
 import Main from '../src/parts/mainContent'
 import Footer from '../src/parts/footer'
 import styles from '../styles/pages.module.scss'
+import User from '../src/types/user'
 
 const HomePage: NextPage = () => {
     return (
@@ -13,6 +16,54 @@ const HomePage: NextPage = () => {
             <Footer />
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<User> = async (context) => {
+    const url = process.env.SITE_URL
+    const {token} = nookies.get(context)
+
+    const moveToLogin = () => {
+        return {
+            redirect: {
+                destination: '/login',
+                permanent: false
+            }
+        }
+    }
+
+    // Verify if the user is authenticated
+
+    if (token !== undefined) {
+        const {isAuthenticated} = await fetch(url + '/api/auth', {
+            method: 'POST',
+            headers: {
+                'Authorization': `${token}`
+            }
+        })
+        .then((response) => response.json())
+
+        // Redirect to login if token is not valid
+
+        if (isAuthenticated !== true) {
+            return moveToLogin()
+        }
+
+        // Get user data if token is valid
+
+        else {
+            const { username, name, image } = jwt.decode(token) as User
+            
+            return {
+                props: {username, name, image}
+            }
+        }
+    }
+
+    // Redirect to login if token does not exists
+
+    else {
+        return moveToLogin()
+    }
 }
 
 export default HomePage
